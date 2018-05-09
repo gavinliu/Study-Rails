f778b751aed0fbff70e4206ca0380df7ea10f3b6
Gavin Liu <gavin6liu@gmail.com>
2018-05-09 11:44:41 +0800

    DELETE

--- blog/app/controllers/api/articles_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::ArticlesController < ApplicationController
-
-  def show
-    @article = Article.all
-
-    render json: @article
-  end
-
-end
--- blog/app/serializers/article_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title
-
-  has_many :comments
-end
